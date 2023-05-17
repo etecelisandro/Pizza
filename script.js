a1993a250de50de1be68c989e0bb92254f5d6b04
Elisandro <111951764+etecelisandro@users.noreply.github.com>
2023-05-17 11:54:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function sim() {
-    alert("VOCÊ ACEITOU DA PIZZAZINHA HOJEEEEE:) ÁS 20H30 VIU");
+    alert("VOCÊ ACEITOU PARTICIPAR DA PIZZAZINHA DE HOJEEEEE:) ÁS 20H30 VIU");
     // redireciona para um URL apos aceitar
-    location.href="https://www.google.com/maps/place/Nova+Sat%C3%A9lite+Esfiharia+e+Pizzaria/@-23.703444,-46.6893455,20.33z/data=!4m9!1m2!2m1!1spadaria+satelite!3m5!1s0x94ce4feec187e35b:0x346ee403c2a56314!8m2!3d-23.7032401!4d-46.6888661!16s%2Fg%2F11t72t143r";
+    location.href="https://www.google.com/maps/place/Nova+Sat%C3%A9lite+Esfiharia+e+Pizzaria/@-23.7033553,-46.6892593,20.75z/data=!4m6!3m5!1s0x94ce4feec187e35b:0x346ee403c2a56314!8m2!3d-23.7032401!4d-46.6888661!16s%2Fg%2F11t72t143r";
 }
 
 function desvia(t) {
